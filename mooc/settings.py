e8a731a02d0f7549c04a46bd374e0970b897e8fb
havoki <tom.havoki@gmail.com>
2014-07-11 23:17:47 -0300

    add app courses

--- mooc/settings.py
+++ mooc/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'mooc.core',
+    'mooc.courses',
 )
 
 MIDDLEWARE_CLASSES = (
--- mooc/urls.py
+++ mooc/urls.py
@@ -4,10 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mooc.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^$', 'mooc.core.views.home', name='home'),
-    url(r'^contato/$', 'mooc.core.views.contact', name='contact'),
+
+    url(r'^', include('mooc.core.urls',namespace='core')),
     url(r'^admin/', include(admin.site.urls)),
 )
