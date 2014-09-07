3c74f844dd8f85ec0d46f9319b0a41bd168ca4ce
havoki <tom.havoki@gmail.com>
2014-09-06 22:47:31 -0300

    add view edit account

--- mooc/accounts/urls.py
+++ mooc/accounts/urls.py
@@ -1,10 +1,18 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
+    
+	url(r'^$', 'mooc.accounts.views.dashboard', 
+        name='dashboard'),
     url(r'^entrar/$', 'django.contrib.auth.views.login', 
         {'template_name': 'accounts/login.html'}, name='login'),
     url(r'^sair/$', 'django.contrib.auth.views.logout', 
         {'next_page': 'core:home'}, name='logout'),
     url(r'^cadastre-se/$', 'mooc.accounts.views.register', 
         name='register'),
+    url(r'^editar/$', 'mooc.accounts.views.edit', 
+        name='edit'),
+ 
+
+
 )
